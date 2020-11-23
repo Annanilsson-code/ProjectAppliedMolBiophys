f32a9e80edc36bd3a347035265cb646938cef14b
Annanilsson-code <74772895+Annanilsson-code@users.noreply.github.com>
2020-11-23 14:46:50 +0100

    Add files via upload
    This script creates a scatter plot from the cc_analysis.py output 2D vectors.

---
+++ results_analysis.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 21 16:52:08 2020
+
+@author: Anna
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+# This script evaluates the result of cc_analysis.py
+# Vectors are saved in results.txt
+
+d = np.loadtxt('results.txt', delimiter="\t")
+
+x = d[0:20,0]
+y = d[0:20,1]
+
+plt.scatter(x,y, c='cyan')
+plt.title('Scatter plot, correlation separation of images in dataset')
+plt.xlabel('Dimension 1')
+plt.ylabel('Dimension 2')
+plt.show()
