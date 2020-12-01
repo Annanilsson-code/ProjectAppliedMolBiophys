9c97a09d72731a681f61f349ff2f884280eee9a6
Annanilsson-code <74772895+Annanilsson-code@users.noreply.github.com>
2020-12-01 20:16:41 +0100

    Update cc_calculation_prot.py

--- cc_calculation_prot.py
+++ cc_calculation_prot.py
@@ -5,18 +5,34 @@ Created on Fri Nov 27 09:42:00 2020
 @author: Anna
 """
 
-from PIL import Image
 import matplotlib.pyplot as plt
-import numpy as np
+import numpy as np 
 import mrcfile 
-
-
 
 mrc1 = mrcfile.mmap('FoilHole_3164980_Data_3165536_3165537_20190104_1142-60848.mrcs', mode='r+')
 
-#Put the mrc files in np arrays. 
-array1 = np.array(mrc1.data)
+n = 1       #n = nr of datasets
 
-# Calculate the mean, std, var of the images
+# Calculate projections
+prot1_mrc = mrcfile.mmap('6yhs.mrc', mode='r+')
+u = np.array(prot1_mrc.data)    # shape = (231,200,214)
 
-# Simulate noise
+sum_x = u.sum(axis=0, keepdims=True)    # shape = (1,200,214)
+sum_y = u.sum(axis=1, keepdims=True)    # shape = (231,1,214)
+sum_z = u.sum(axis=2, keepdims=True)    # shape = (231,200,1)
+
+
+# Make a histogram of the experimental data. 
+exp_data = np.array(mrc1.data)
+exp_data_reshaped = exp_data.reshape(n,-1)
+
+    # With numpy
+np.histogram(exp_data_reshaped, bins = 'auto')
+hist, bins = np.histogram(exp_data_reshaped, bins = 'auto')
+print(hist)
+print(bins)
+
+    # With matplotlib
+plt.hist(exp_data_reshaped, bins='auto')
+plt.title('Histogram of exp data')
+plt.show()
