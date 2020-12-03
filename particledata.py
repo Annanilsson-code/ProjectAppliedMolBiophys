b840c3b9c891493d093578c389e29c6348bdc120
YlvaJansson <74774648+YlvaJansson@users.noreply.github.com>
2020-12-03 14:01:14 +0100

    Update particledata.py

--- particledata.py
+++ particledata.py
@@ -32,7 +32,7 @@ noisy_proj = np.zeros((n, size.shape[0], size.shape[1]))
 classes = np.zeros((n))
 for i in range(n):
     c = np.random.randint(0, nclasses)
-    gaussian = np.random.normal(mean, std, proj[c].shape)*12
+    gaussian = np.random.normal(mean, std, proj[c].shape)*10
     noisy_proj[i] = (proj[c]*0.001) + gaussian
     classes[i] = c
 np.save('classes.npy',classes)
@@ -40,14 +40,16 @@ np.save('classes.npy',classes)
 #import experimental data for statistical noise distrubution 
 with mrcfile.open('FoilHole_3164980_Data_3165536_3165537_20190104_1142-60848.mrcs', 'r+') as mrc:
      arr_exp = np.array(mrc.data[1])
-reshape_exp = arr_exp.reshape((-1,1))
+exp_selct = arr_exp[0:200,0:200] #same size as projections 
+reshape_exp = exp_selct.reshape((-1,1))
 
 # compare noise distrubution 
 oneprojnoise = noisy_proj[60]
 reshape_proj = oneprojnoise.reshape(-1,1)
-plt.hist(reshape_exp, bins='auto', alpha=0.5, label="Exp")
-plt.hist(reshape_proj, bins='auto', alpha=0.5, label="Noise", stacked='true')
-
+plt.hist(reshape_exp, bins='auto', alpha=0.5, label="Experimental data")
+plt.hist(reshape_proj, bins='auto', alpha=0.5, label="Generated projections", stacked='true')
+plt.title('Pixel intensities of projected and experimental data')
+plt.legend(loc='upper right')
 plt.figure()
 plt.imshow(oneprojnoise)
 plt.figure()
